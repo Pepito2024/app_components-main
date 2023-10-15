11efcc37ebba177227351b059436ea6a7db8ab27
Pepito2024 <021370b@utea.edu.pe>
2023-10-15 12:38:38 -0500

    final

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class InputPage extends StatelessWidget {
-  const InputPage({super.key});
+class InputPage extends StatefulWidget {
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  bool isInvisible = true;
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +33,99 @@ class InputPage extends StatelessWidget {
                 icon: Icon(Icons.location_on),
                 suffixIcon: Icon(Icons.check),
                 prefixIcon: Icon(Icons.star),
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Buscar producto",
+                prefixIcon: Icon(Icons.search),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                    width: 3,
+                  ),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: Colors.black54,
+                    width: 5,
+                  ),
+                ),
+              ),
+              onChanged: (value) {
+                print(value);
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.75),
+                  offset: Offset(4, 4),
+                  blurRadius: 14,
+                ),
+              ]),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Ingrese su nombre completo",
+                  hintStyle: GoogleFonts.poppins(
+                    fontSize: 15,
+                    color: Colors.black38,
+                  ),
+                  fillColor: Colors.white,
+                  filled: true,
+                  suffixIcon: Container(
+                    margin: EdgeInsets.all(3),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.pinkAccent,
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(14),
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(14),
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              obscureText: isInvisible,
+              // obscuringCharacter: "X",
+              decoration: InputDecoration(
+                hintText: "Contraseña",
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    isInvisible = !isInvisible;
+                    setState(() {});
+                  },
+                  icon: isInvisible
+                      ? Icon(Icons.remove_red_eye)
+                      : Icon(Icons.remove_red_eye_outlined),
+                ),
               ),
             ),
           ],
